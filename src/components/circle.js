d719d24d16f7fcca159077ec33e3aac05f73ab89
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-01 14:18:40 +0530

    change

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
-// import using require
+import Shape from "./shape";
 
-// declare class
-
-
-// export class using module.exports
+//export default class Circle extends Shape
+module.exports = class Circle extends Shape {
+  constructor() {
+    //first line of subclass constructor should call superclass constructor with appropriate parameters
+    super();
+  }
+  calculateArea() {
+    console.log("Area of circle");
+    return 3.14 * 5 * 5;
+  }
+};
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
 
-// export class using module.exports
+// export class using module.exports//export default class Shape
+module.exports = class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("Drawing a shape");
+  }
+  calculateArea() {
+    console.log("don't know area of uknown shape");
+  }
+};
